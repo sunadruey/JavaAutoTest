6b13313a8114866d630057c24690151beb3b3bd6
ShuangmeiZhao <sunadruey@163.com>
2021-12-05 16:18:37 +0800

    查看运行的顺序和结果可见suite先运行 ，注意suite配置文件

--- Chapter5/src/main/java/com/course/testng/suite/LoginTest.java
+++ Chapter5/src/main/java/com/course/testng/suite/LoginTest.java
@@ -1,4 +1,12 @@
 package com.course.testng.suite;
+
+import org.testng.annotations.Test;
+
 //逻辑控制类
 public class LoginTest {
+
+    @Test
+    public  void  loginTaoBao(){
+        System.out.println("淘宝登录成功");
+    }
 }
--- Chapter5/src/main/java/com/course/testng/suite/SuiteConfig.java
+++ Chapter5/src/main/java/com/course/testng/suite/SuiteConfig.java
@@ -1,4 +1,18 @@
 package com.course.testng.suite;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
 //所有测试套件的配置类
 public class SuiteConfig {
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("before Suite运行了");
+    }
+
+     @AfterSuite
+    public void afterSuite(){
+        System.out.println("after Suite运行了");
+    }
 }
