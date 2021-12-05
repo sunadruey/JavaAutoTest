435cad80f1a5fa0c95f67253d17077947d8adcb0
ShuangmeiZhao <sunadruey@163.com>
2021-12-05 15:41:18 +0800

    BeforeClass和AfterClass的使用

--- Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
+++ Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -1,8 +1,6 @@
 package com.course.testng;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BasicAnnotation {
     //最基本的注解，用来把方法标记为测试的一部分
@@ -24,8 +22,20 @@ public class BasicAnnotation {
     @AfterMethod
     public void afterMethod(){
         System.out.println("afterMethod这是在测试方法之后运行的");
-
-
     }
+
+
+// 使用场景：类正式运行前是不是需要注册写对象，静态东西。赋值什么的，静态方法 变量赋值，这样其他方法就可以用了
+    @BeforeClass
+    public void  beforeClass(){
+        System.out.println("BeforeClass这是在类运行之前运行的方法");
+    }
+
+    @AfterClass
+    public  void  afterClass(){
+        System.out.println("AfterClass这是在类运行之后运行的方法");
+    }
+
+
 
 }
