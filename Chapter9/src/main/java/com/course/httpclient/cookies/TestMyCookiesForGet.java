a2863ccb7aed86fa3d6689117644d519b7cf8266
ShuangmeiZhao <sunadruey@163.com>
2021-12-10 17:43:15 +0800

    get请求携带cookie

--- Chapter9/src/main/java/com/course/httpclient/cookies/TestMyCookiesForGet.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/TestMyCookiesForGet.java
@@ -1,5 +1,7 @@
 package com.course.httpclient.cookies;
 
+
+import org.apache.commons.httpclient.Cookie;
 import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -15,6 +17,7 @@ public class TestMyCookiesForGet {
 
     private String url;
     private ResourceBundle bundle;
+    private Cookie[] cookies;
 
 
     @BeforeTest
@@ -26,38 +29,79 @@ public class TestMyCookiesForGet {
     @Test
     public void testGetCookies() throws IOException {
         String result;
-        String uri=bundle.getString("getCookies.uri");
-        String  testUrl=this.url+uri;
+        String uri = bundle.getString("getCookies.uri");
+        String testUrl = this.url + uri;
         // Create an instance of HttpClient.
         HttpClient client = new HttpClient();
         // Create a method instance.
         GetMethod method = new GetMethod(testUrl);
 
-
         method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
                 new DefaultHttpMethodRetryHandler(3, false));
-        int statusCode = client.executeMethod(method);
+        client.executeMethod(method);
+
+        this.cookies = client.getState().getCookies();
+        if (cookies.length == 0) {
+            //如果获取cookies为空
+            System.out.println("None");
+        } else {
+            for (int i = 0; i < cookies.length; i++) {
+                System.out.println(cookies.length);
+                String value= cookies[i].getValue();
+                String name= cookies[i].getName();
+                System.out.println("Cookie-name  : " + name);
+                System.out.println("Cookie-value : " + value);
+
+                String a = cookies[i].toString();
+                //获取cookies里面所有信息
+                System.out.println();
+                System.out.println(a);
+            }
+
+
+//       System.out.println(Arrays.toString(client.getState().getCookies()));
 
             // Read the response body.
-         byte[] responseBody = method.getResponseBody();
+            byte[] responseBody = method.getResponseBody();
 
             // Deal with the response.
             // Use caution: ensure correct character encoding and is not binary data
-        System.out.println(new String(responseBody));
-
-        //获取cookies信息
-//        method.getParams().setCookiePolicy(CookiePolicy.IGNORE_COOKIES);
-//        method.setRequestHeader("Cookie", "special-cookie=value");
+            System.out.println(new String(responseBody));
 
 
+        }
+    }
+    @Test(dependsOnMethods = {"testGetCookies"})
+    public void testGetWithCookies() throws IOException {
+        String result;
+        String uri = bundle.getString("getWithCookies.uri");
+        String testUrl = this.url + uri;
+        // Create an instance of HttpClient.
+        HttpClient client = new HttpClient();
+        System.out.println("url  +  "+ testUrl);
 
-//        CookieStore store = client.getCookiesStore();
-//        List<Cookie> cookieList = store.getCookies();
-//        for(Cookie cookie: cookieList){
-//            String name=cookie.getName();
-//            String value =cookie.getValue();
-//            System.out.println("Cookie name =" +name +";cookie value = "+ value);
+        // Create a method instance.
+        GetMethod method = new GetMethod(testUrl);
+        client.executeMethod(method);
+        //设置cookis
+
+
+
+        //获取状态码
+        int statusCode=method.getStatusLine().getStatusCode();
+        System.out.println("statusCode  +  "+ statusCode);
+        if(statusCode ==200){
+
+            byte[] responseBody = method.getResponseBody();
+            // Deal with the response.
+            // Use caution: ensure correct character encoding and is not binary data
+            System.out.println(new String(responseBody));
+
         }
 
+
+
+
     }
+}
 
