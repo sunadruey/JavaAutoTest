70ce0dc1634a76727a9c04afdf7ec1411b4ee762
ShuangmeiZhao <sunadruey@163.com>
2021-12-11 18:21:41 +0800

    post with  cookie

--- Chapter9/src/main/java/com/course/httpclient/cookies/MyCookieForPost.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookieForPost.java
@@ -1,10 +1,15 @@
 package com.course.httpclient.cookies;
+
 import org.apache.commons.httpclient.Cookie;
 import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.apache.commons.httpclient.params.HttpMethodParams;
-import org.apache.http.client.CookieStore;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -12,18 +17,19 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class TestMyCookiesForGet {
+public class MyCookieForPost {
 
     private String url;
     private ResourceBundle bundle;
     private Cookie[] cookies;
-    private CookieStore store;
+
 
     @BeforeTest
-    public  void  beforeTest(){
-        bundle =ResourceBundle.getBundle("application", Locale.CHINA);
+    public void beforeTest() {
+        bundle = ResourceBundle.getBundle("application", Locale.CHINA);
         url = bundle.getString("test.url");
     }
+
     @Test
     public void testGetCookies() throws IOException {
         String result;
@@ -45,8 +51,8 @@ public class TestMyCookiesForGet {
         } else {
             for (int i = 0; i < cookies.length; i++) {
                 System.out.println(cookies.length);
-                String value= cookies[i].getValue();
-                String name= cookies[i].getName();
+                String value = cookies[i].getValue();
+                String name = cookies[i].getName();
                 System.out.println("Cookie-name  : " + name);
                 System.out.println("Cookie-value : " + value);
 
@@ -65,12 +71,32 @@ public class TestMyCookiesForGet {
 
         }
     }
+
     @Test(dependsOnMethods = {"testGetCookies"})
-    public <List> void testGetWithCookies() throws IOException {
-        String uri = bundle.getString("getWithCookies.uri");
+    public void testPostMethods() throws IOException {
+        String uri = bundle.getString("postWithCookies.uri");
         String testUrl = this.url + uri;
         // Create an instance of HttpClient.
         HttpClient client = new HttpClient();
+        System.out.println("url  +  " + testUrl);
+        // Create a method instance.
+        PostMethod method = new PostMethod(testUrl);
+
+        //添加参数
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name","huhansan");
+        jsonObject.put("age","18");
+        String  toJson = jsonObject.toString();
+        RequestEntity se = new StringRequestEntity(toJson ,"application/json" ,"UTF-8");
+        method.setRequestEntity(se);
+
+        //添加请求头信息
+        method.setRequestHeader("Content-Type","application/json");
+        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler());
+
+        //设置超时时间
+        method.getParams().setParameter(HttpMethodParams.SO_TIMEOUT, 5000);
+
 
         //设置cookis
         System.out.println("==========Cookies============");
@@ -79,33 +105,27 @@ public class TestMyCookiesForGet {
             System.out.println(++j + ":   " + c);
         }
         client.getState().addCookies(this.cookies);
-
-
-
         System.out.println("url  +  "+ testUrl);
         // Create a method instance.
-        GetMethod method = new GetMethod(testUrl);
         client.executeMethod(method);
 
         //获取状态码
         int statusCode=method.getStatusLine().getStatusCode();
         System.out.println("statusCode  +  "+ statusCode);
-        if(statusCode ==200){
+        if(statusCode ==200) {
 
-            byte[] responseBody = method.getResponseBody();
-
-            // Deal with the response.
-            // Use caution: ensure correct character encoding and is not binary data
-            System.out.println(new String(responseBody));
-
+        //返回结果
+            String response =method.getResponseBodyAsString();
+            JSONObject result = new JSONObject(new String(response));
+            System.out.println("返回的结果的json形式： "+ result);
+            String success = (String)result.get("huhansan");
+            Assert.assertEquals("success",success);
 
         }
 
-
-
-
-    }
-
-
+        }
 }
+
+
+
 
